0de4b66bf8a13a6d70662ee2e53a7cb27e2a25e8
sny-nkmr <u110864b@ecs.osaka-u.ac.jp>
2015-11-13 15:59:43 +0900

    #7 とりあえず絶対に使いそうな情報を保持するクラス

---
+++ group06zero/EnemyRobot.java
@@ -0,0 +1,46 @@
+package group06zero;
+import robocode.*;
+import java.awt.geom.Point2D;
+
+public class EnemyRobot{
+
+	static private String name;
+	static private double hp;
+	static private double x,y;
+	static private double heading;
+
+	EnemyRobot(ScannedRobotEvent e,Group06zerogouki self){
+		name = e.getName();
+		hp = e.getEnergy();
+		heading = e.getHeading();
+		double absBearing=e.getBearingRadians()+self.getHeadingRadians();
+		x = self.getX()+e.getDistance()*Math.sin(absBearing);
+		y = self.getY()+e.getDistance()*Math.cos(absBearing);
+		heading = e.getHeadingRadians();
+	}
+
+	public void Update(ScannedRobotEvent e,Group06zerogouki self){
+		hp = e.getEnergy();
+		heading = e.getHeading();
+		double absBearing=e.getBearingRadians()+self.getHeadingRadians();
+		x = self.getX()+e.getDistance()*Math.sin(absBearing);
+		y = self.getY()+e.getDistance()*Math.cos(absBearing);
+		heading = e.getHeadingRadians();
+	}
+
+	public String getName(){
+		return name;
+	}
+	public double getHp(){
+		return hp;
+	}
+	public double getX(){
+		return x;
+	}
+	public double getY(){
+		return y;
+	}
+	public double getHeading(){
+		return heading;
+	}
+}
