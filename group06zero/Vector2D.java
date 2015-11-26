68491e854346b724a84f7ed0d2de6920347e55a9
佐々木智広 <sasakitomohiro@sasakitomohiro-no-MacBook-Pro.local>
2015-11-27 02:45:09 +0900

    Vector2Dの修正

--- group06zero/Vector2D.java
+++ group06zero/Vector2D.java
@@ -17,7 +17,11 @@ public class Vector2D {
 	}
 
 	public double getMagnitude() {
+<<<<<<< Updated upstream
 		return math.sqrt(this.x * this.x + this.y * this.y);
+=======
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+>>>>>>> Stashed changes
 	}
 
 	public Vector2D normalized() {
@@ -33,6 +37,15 @@ public class Vector2D {
 		return this;
 	}
 
+<<<<<<< Updated upstream
+=======
+	public Vector2D sub(Vector2D vector) {
+		this.x -= vector.x;
+		this.y -= vector.y;
+		return this;
+	}
+
+>>>>>>> Stashed changes
 	public Vector2D mul(double num) {
 		this.x *= num;
 		this.y *= num;
@@ -43,6 +56,13 @@ public class Vector2D {
 		return new Vector2D(v1.x + v2.x, v1.y + v2.y);
 	}
 
+<<<<<<< Updated upstream
+=======
+	public static Vector2D Sub(Vector2D v1, Vector2D v2) {
+		return new Vector2D(v1.x - v2.x, v1.y - v2.y);
+	}
+
+>>>>>>> Stashed changes
 	public static Vector2D Normalize(Vector2D vector) {
 		double magnitude = vector.getMagnitude();
 		double x = vector.x / magnitude;
@@ -50,9 +70,21 @@ public class Vector2D {
 		return new Vector2D(x, y);
 	}
 
+<<<<<<< Updated upstream
 	public static Vector2D GetDirectionalVectorFromDegree(double degree) {
 		double x = math.sin(degree);
 		double y = math.cos(degree);
+=======
+	public static Vector2D GetDirectionalVectorFromDegree(double angle) {
+		double x = Math.sin(Math.toRadians(angle));
+		double y = Math.cos(Math.toRadians(angle));
+		return new Vector2D(x, y);
+	}
+
+	public static Vector2D GetRotatedVector2D(Vector2D vector, double angle) {
+		double x = vector.x * Math.cos(Math.toRadians(angle)) + vector.y * Math.sin(Math.toRadians(angle));
+		double y = -vector.x * Math.sin(Math.toRadians(angle)) + vector.y * Math.cos(Math.toRadians(angle));
+>>>>>>> Stashed changes
 		return new Vector2D(x, y);
 	}
 }
